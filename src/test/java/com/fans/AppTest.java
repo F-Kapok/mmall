ae8c8f54b5afa262613e9225c66aff377424956a
F-Kapok <5219824@qq.com>
2018-11-22 14:35:14 +0800

    first commit

---
+++ src/test/java/com/fans/AppTest.java
@@ -0,0 +1,20 @@
+package com.fans;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
