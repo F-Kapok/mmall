a26955f07e2b99f4f8db79d9f175884667152e98
F-Kapok <5219824@qq.com>
2018-12-10 16:19:45 +0800

    分类管理模块开发

---
+++ src/main/java/com/fans/controller/backend/CateGoryManageController.java
@@ -0,0 +1,11 @@
+package com.fans.controller.backend;
+
+/**
+ * @ClassName CateGoryManageController
+ * @Description: TODO 节点管理控制层
+ * @Author fan
+ * @Date 2018-12-10 10:06
+ * @Version 1.0
+ **/
+public class CateGoryManageController {
+}
---
+++ src/main/java/com/fans/service/interfaces/ICateGoryService.java
@@ -0,0 +1,11 @@
+package com.fans.service.interfaces;
+
+/**
+ * @InterfaceName ICateGoryService
+ * @Description: TODO 节点服务层
+ * @Author fan
+ * @Date 2018-12-10 13:38
+ * @Version 1.0
+ **/
+public interface ICateGoryService {
+}
