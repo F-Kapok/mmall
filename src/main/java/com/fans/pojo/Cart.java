d92b6f0051f68642759000a19c2fe399b0361ebe
F-Kapok <5219824@qq.com>
2019-01-08 10:11:19 +0800

    delete

--- src/main/java/com/fans/pojo/Cart.java
+++ src/main/java/com/fans/pojo/Cart.java
@@ -2,19 +2,40 @@ package com.fans.pojo;
 
 import java.util.Date;
 
-public class MmallCart {
+public class Cart {
+    /**
+     * 购物车表id
+     */
     private Integer id;
 
+    /**
+     * 对应mmall_user表id
+     */
     private Integer userId;
 
+    /**
+     * 对应mall_product表id
+     */
     private Integer productId;
 
+    /**
+     * 数量
+     */
     private Integer quantity;
 
+    /**
+     * 是否已选择 1-已勾选   0-未勾选
+     */
     private Integer checked;
 
+    /**
+     * 创建时间
+     */
     private Date createTime;
 
+    /**
+     * 修改时间
+     */
     private Date updateTime;
 
     public Integer getId() {
